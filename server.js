940dd11f3a76030f169e203cc9121d0b303bbb5b
mcmloke-del <mcmloke@gmail.com>
2026-07-19 20:15:51 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+app.get('/', (req, res) => {
+  res.send('Mova Socket Server is Running on Render!');
+});
+
+io.on('connection', (socket) => {
+  console.log('مستخدم جديد اتصل:', socket.id);
+
+  socket.on('update-location', (data) => {
+    io.emit(`location-${data.rideId}`, data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('انقطع الاتصال:', socket.id);
+  });
+});
+
+// منصة Render تستخدم منافذ متغيرة، لذلك هذا السطر مهم جداً
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
